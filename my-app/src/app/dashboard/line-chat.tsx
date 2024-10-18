9d55d01657f103a60afed830c3ff5a8fb7df4fec
Avinash Markad <markadavi3719@gmail.com>
2024-10-18 14:09:44 +0530

    file colocation

---
+++ my-app/src/app/dashboard/line-chat.tsx
@@ -0,0 +1,7 @@
+export default function lineChart(){
+    return(
+        <div>
+            <h1>Line Chart</h1>
+        </div>
+    );
+}
---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+function barChart(){
+    return(
+        <div>
+            <h1>bar chart</h1>
+        </div>
+    );
+}
+
+// Unhandled Runtime Error
+// Error: The default export is not a React Component in page: "/dashboard"
+
+// we need to follow this
+
+export default function dashboard(){
+    return(
+        <div>
+        <h1>dashboard</h1>
+        </div>
+    );
+}
