9bb850338e866aabddec8fd7303801ba45c55a46
Avinash Markad <markadavi3719@gmail.com>
2024-10-18 14:51:07 +0530

    private folder

---
+++ my-app/src/app/_private/page.tsx
@@ -0,0 +1,8 @@
+export default function _private(){
+    return(
+        <div>
+            <h1>this is private route/file</h1>
+            <p>Access to this route/file is restricted. Please ensure you have the necessary permissions to proceed.</p>
+        </div>
+    );
+}
